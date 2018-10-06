bc394d5d676dde68189ad5f117877aeab19c9d69
Code Next <codenext@Codes-MacBook-Air-4.local>
2018-10-06 13:18:10 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,14 @@
 // Example
-let message = "Hello World!";
+let message = "Hello Bri!";
 console.log(message);
 
 // Type your code below!
+let bDay = 20;
+let computerNumber = 4;
+let numberOfSibs = 5;
+let message = "I love you"
+let aMessage = "taco tuesday"
+let bMessage = "what"
+let havingInsta = true;
+let friendsWithGaberial = true;
+let smashingApples = undefined;
