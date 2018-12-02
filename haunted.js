16adb345d0ebde76d46dfb7dad4d98ee2b318110
student <student@admins-MBP.attlocal.net>
2018-12-01 19:43:23 -0800

    Update haunted.js
    updated haunted.js

--- haunted.js
+++ haunted.js
@@ -7,7 +7,7 @@ let player = {
   intellect: 98,
   strength: 100,
   charm: 80,
-  weapon: "AK47",
+  weapon: "handgun",
   item: "phone",
 };
 
@@ -15,28 +15,21 @@ console.log();
 console.log("*** WELCOME TO THE HAUNTED HOUSE PARTY ***");
 player.name = READLINE.question("What is your name?");
 console.log(`${player.name} hears that there is a party tonight`);
-let enterHouse = READLINE.question("Will you go to the party (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
-  player.courage++;
+let partyTime = READLINE.question("Will you go to the party (yes or no) ");
+
+if(partyTime== "y" || partyTime == "yes") {
+
+} if(partyTime== "n" || partyTime == "no") {
+
+}player.courage++;
   console.log(`${player.name} gets their ${player.item} and leaves home, then heads to the party...`);
+  let kitchen = READLINE.question("Go to kitchen to get a drink? (yes or no) ");
+  if(kitchen== "y" || enterHouse== "yes" ){
+
+  } if(kitchen== "n" || kitchen== "no") {
+
+  }
   // continue the story
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 } else {
   player.intellect++;
   console.log(`${player.name} decides not to open the door. However...`);
